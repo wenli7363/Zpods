5f3e9df14897fe521d811edb71f57160693a101d
wenli7363 <cjy0415@qq.com>
2023-11-02 00:44:07 +0800

    finish local interface

--- client/gui/mainwindow.cpp
+++ client/gui/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <network.h>
+#include "zpods_lib.h"
+#include "termios.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setWindowTitle("Zpods");
 }
 
 MainWindow::~MainWindow()
